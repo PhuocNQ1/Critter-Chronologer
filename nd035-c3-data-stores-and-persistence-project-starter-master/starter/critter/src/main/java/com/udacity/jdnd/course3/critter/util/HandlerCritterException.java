97eefcc60d48f8590bcf2d6a68e562ad104823bd
PhuocNQ3 <phuocnguyen.2272000@gmail.com>
2023-11-04 18:22:15 +0700

    Init project

--- nd035-c3-data-stores-and-persistence-project-starter-master/starter/critter/src/main/java/com/udacity/jdnd/course3/critter/util/HandlerCritterException.java
+++
@@ -1,7 +0,0 @@
-package com.udacity.jdnd.course3.critter.util;
-
-public class HandlerCritterException extends RuntimeException{
-    public HandlerCritterException(String message) {
-        super(message);
-    }
-}
